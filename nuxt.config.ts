65bac0e400132dff2ecd1e56a0a9f7e1b8996769
EvgenyWas <evgeniywas@gmail.com>
2025-01-01 18:33:54 +0000

    feat: add swr 1 day to home page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-07-30',
+  compatibilityDate: "2024-07-30",
 
   // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
   future: { compatibilityVersion: 4 },
 
   // https://nuxt.com/modules
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui',
-    '@nuxthub/core',
-    '@vueuse/nuxt',
-  ],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxthub/core", "@vueuse/nuxt"],
 
   // https://hub.nuxt.com/docs/getting-started/installation#options
   hub: {
@@ -19,18 +14,25 @@ export default defineNuxtConfig({
   },
 
   icon: {
-    serverBundle: 'remote',
+    serverBundle: "remote",
   },
 
   // https://eslint.nuxt.com
   eslint: {
     config: {
       stylistic: {
-        quotes: 'single',
+        quotes: "single",
       },
     },
   },
 
   // https://devtools.nuxt.com
   devtools: { enabled: true },
-})
+
+  // https://nuxt.com/docs/guide/concepts/rendering#route-rules
+  routeRules: {
+    "/": {
+      swr: 60 * 60 * 24, // 1 day
+    },
+  },
+});
